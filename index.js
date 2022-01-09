d6531a9504590a58868a46f4a30270a2f13836b6
venomiele <atrandafir35@yahoo.com>
2022-01-09 14:12:07 -0800

    added logic for the validation errors plus succes cases

--- index.js
+++ index.js
@@ -15,13 +15,55 @@ function checkInputs() {
     let emailValue = email.value.trim();
     let pass1Value = pass1.value.trim();
     let pass2Value = pass2.value.trim();
+    let section = document.querySelectorAll(".form-validation");
 
-    let section = document.querySelector(".form-validation");
-  if(userValue  == "") {
-section.classList.add("error-msg");
-section.classList.remove("succes");
-  } else {
-      section.classList.remove("error-msg");
-      section.classList.add("succes");
+    if(userValue == "") {
+      setErrorFor(user, "Username can't be blank !");
+    } else {
+      setSuccessFor(user);
+    }
+
+    if(emailValue == "") {
+      setErrorFor(email, "Email can't be empty")
+    } else if (!validateEmail(emailValue)){
+      setErrorFor(email, "Email is not valid")
+   } else {
+      setSuccessFor(email);
+    }
+
+    if(pass1Value == "") {
+      setErrorFor(pass1, "Password can't be empty")
+    } else {
+      setSuccessFor(pass1);
+    }
+
+    if(pass2Value == "") {
+      setErrorFor(pass2, "Password can't be empty")
+    } else {
+      setSuccessFor(pass2);
+    }
+
   }
-}
+
+  // Error Case function
+
+  function setErrorFor(input, message) {
+    let formControl = input.parentElement;
+    let small = formControl.querySelector(".error");
+    small.innerText = message;
+    formControl.className = "form-validation error-msg";
+  }
+
+  // Success Case function
+
+  function setSuccessFor(input) {
+    let formControl = input.parentElement;
+    formControl.className = "form-validation success";
+  }
+  
+//Form validaton RegEx
+
+  function validateEmail(elementValue){      
+    var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailPattern.test(elementValue); 
+  } 
